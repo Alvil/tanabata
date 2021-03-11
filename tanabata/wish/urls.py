961054ae3a5b089ea29f81dedbcdcdd54fd97748
Alvil <alvil.balbuena@gmail.com>
2021-03-11 21:24:17 +0800

    Create a url for WishCreateView.

--- tanabata/wish/urls.py
+++ tanabata/wish/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from .views import WishCreateView
 from .views import WishListView
+
 
 app_name = 'wish'
 
 urlpatterns = [
-    path('', WishListView.as_view(), name='landing_page')
+    path('', WishListView.as_view(), name='landing-page'),
+    path('wish/', WishCreateView.as_view(), name='create-wish'),
 ]
