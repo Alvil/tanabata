6b62d77007bd94019d55c0ad01496bb4e7f2848a
Alvil <alvil.balbuena@gmail.com>
2021-03-11 20:38:28 +0800

    Create Wish model.

--- tanabata/wish/models.py
+++ tanabata/wish/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Wish(models.Model):
+    wish = models.CharField(max_length=100)
+    times_wished = models.IntegerField(default=1)
